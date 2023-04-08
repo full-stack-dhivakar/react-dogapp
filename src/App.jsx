2c26c736c54c2f4fb8d31b341e27078efd25ec99
full-stack-dhivakar <106915835+full-stack-dhivakar@users.noreply.github.com>
2023-04-08 18:58:47 +0530

    draft-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <>
-   <h1>Hello</h1>
+   <h1>Adopt Me!</h1>
+  <Pet name ="Luna" animal="dog" breed="Havanese"/>
+  <Pet name ="Luna" animal="dog" breed="Havanese"/>
+  <Pet name ="Luna" animal="dog" breed="Havanese"/>
    </>
   )
 }
